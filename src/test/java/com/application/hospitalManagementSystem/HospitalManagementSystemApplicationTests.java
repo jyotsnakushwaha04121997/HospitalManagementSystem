8c94d1b2c712d751a241257efddc1e7d5d767490
jyotsnakushwaha04121997 <jyotsnakushwaha04121997@gmail.com>
2026-06-11 18:03:41 +0530

    second practise Spring Boot project

---
+++ src/test/java/com/application/hospitalManagementSystem/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.hospitalManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
